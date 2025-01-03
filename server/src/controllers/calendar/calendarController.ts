c1bf8904c31850f958dbd0ef4b57c56fd7bca475
saksham <phantomdx316@gmail.com>
2025-01-03 09:37:21 +0530

    added the logs

--- server/src/controllers/calendar/calendarController.ts
+++ server/src/controllers/calendar/calendarController.ts
@@ -98,7 +98,7 @@ export const watchCalendarEvents = asyncErrorHandler(async (req, res, next) => {
       }
       //
       const eventRes = await fetchEvents(googleOAuthClient);
-
+      console.log("calendar data is here fools", eventRes.data);
       if (eventRes.status === 200 && eventRes.statusText === "OK") {
         if (eventRes.data && eventRes.data.items) {
           let data = eventRes.data.items.map((item) => {
